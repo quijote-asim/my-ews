383442f1ab78e592a6a7b310d92c8ecc68df924b
quijote-asim <quijote.libre@posteo.es>
2025-07-25 16:14:04 +0200

    Añadida mi estructura org-todo

--- custom.el
+++ custom.el
@@ -6,7 +6,15 @@
  ;; If there is more than one, they won't work right.
  '(denote-link-description-format #'ews-denote-link-description-title-case nil nil "Customized with use-package denote")
  '(menu-bar-mode nil)
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(balanced-windows biblio citar-denote consult-notes denote-explore
+		      denote-journal denote-org ef-themes elfeed-org
+		      emms fountain-mode helpful lorem-ipsum
+		      marginalia markdown-mode mixed-pitch
+		      modus-themes nov olivetti openwith orderless
+		      org-appear org-fragtog org-modern org-web-tools
+		      ox-epub powerthesaurus spacious-padding
+		      titlecase undo-tree vertico writegood-mode))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
