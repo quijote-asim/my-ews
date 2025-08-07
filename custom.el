80a0b427bf9363eaca7b450afd2e2793fbb9fd11
quijote-asim <asim@acer>
2025-08-07 14:02:54 +0200

    Cambiada funete al cambiar a Debian

--- custom.el
+++ custom.el
@@ -5,10 +5,19 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(menu-bar-mode nil)
+ '(package-selected-packages
+   '(balanced-windows biblio citar-denote consult-notes denote-explore
+		      denote-journal denote-org denote-sequence
+		      ef-themes elfeed-org emms fountain-mode helpful
+		      lorem-ipsum marginalia markdown-mode mixed-pitch
+		      modus-themes nov olivetti openwith orderless
+		      org-appear org-fragtog org-modern org-web-tools
+		      ox-epub ox-hugo spacious-padding titlecase
+		      vertico vundo writegood-mode))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Cascadia Code NF" :foundry "SAJA" :slant normal :weight regular :height 132 :width normal)))))
+ '(default ((t (:family "Liberation Mono" :foundry "1ASC" :slant normal :weight regular :height 111 :width normal)))))
